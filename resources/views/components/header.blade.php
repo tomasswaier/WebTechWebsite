d1c44474430009a8c2ba587ef380f570f8816c97
stilecdaníel <stilecdaniel@gmail.com>
2025-04-20 19:12:45 +0200

    product detail setup

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,9 @@
 <a href="{{ url('/')}}" id="logo" class="w-auto h-14 bg-neutral-300 rounded-2xl mr-auto m-5">
-    <img src="icons/logo.png" alt="logo" class="h-full">
+    <img src="{{asset('icons/logo.png')}}" alt="logo" class="h-full">
 </a>
 <button id="hamburger_btn" type="button" data-collapse-toggle="hamburger_default" aria-controls="hamburger_default" aria-expanded="false" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
     <span class="sr-only">Open main menu</span>
-    <img src="icons/hamburgerIcon.png" alt="hamburger icon" class="max-h-12 md:mb-0">
+    <img src="{{asset('icons/hamburgerIcon.png')}}" alt="hamburger icon" class="max-h-12 md:mb-0">
 </button>
 <div id="hamburger_default" class="absolute hidden bg-white mt-5 md:mt-0 top-full right-0 w-full z-50 md:static md:flex md:w-auto md:mr-3">
     <ul class="flex flex-col z-50 md:flex-row space-y-3 md:space-y-0 p-4 md:p-0 bg-white border border-gray-500 md:mt-0 md:border-0 md:bg-white md:space-x-2">
@@ -11,20 +11,20 @@
             <form method="post" action="#">
                 <input type="search" name="search_query" placeholder="Search.." class="block ml-auto w-full md:w-96 rounded-lg border-black border p-1 focus:outline-none pl-5">
                 <button type="submit" class="absolute top-0 z-20 end-0 rounded-e-lg bg-black p-2 focus:ring-1 border border-black">
-                    <img src="icons/search.png" alt="search-icon" class="w-4 h-4">
+                    <img src="{{asset('icons/search.png')}}" alt="search-icon" class="w-4 h-4">
                 </button>
             </form>
         </li>
         <li class="block">
             <a href="{{ url('cartOverview')}}" class="flex w-full justify-center space-x-3 md:space-x-0 border md:border-transparent bg-white md:bg-transparent border-black rounded-lg">
-                <img src="icons/shoppingCart.png" alt="shopping cart icon" class="max-h-10 mb-2 md:mb-0">
+                <img src="{{asset('icons/shoppingCart.png')}}" alt="shopping cart icon" class="max-h-10 mb-2 md:mb-0">
                 <span class="flex md:hidden self-center">Cart</span>
             </a>
         </li>
         @if(Auth::check())
             <li class="block">
                 <a href="{{ url('/profile')}}" class="flex w-full justify-center space-x-3 md:space-x-0 border md:border-transparent bg-white md:bg-transparent border-black rounded-lg">
-                    <img src="icons/person.png" alt="user page icon" class="max-h-10 mb-2 md:mb-0">
+                    <img src="{{asset('icons/person.png')}}" alt="user page icon" class="max-h-10 mb-2 md:mb-0">
                     <span class="flex md:hidden self-center">User Profile</span>
                 </a>
             </li>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/detail/{id}', [ProductController::class, 'detail'])->name('product.detail');
 
 //Route::get('/dashboard', function () {
 //    return view('dashboard');
